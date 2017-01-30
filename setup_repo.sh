03e122531bd24d3d65445f38103b2b6e1187f4bb
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-01-30 09:47:48 -0500

    Fix link to github

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # add the correct remote repositories
-git_repo="https://github.com/skulumani/2017_RD_poster"
+git_repo="git@github.com:skulumani/2017_RD_poster.git"
 bb_repo="git@bitbucket.org:shankarkulumani/2017_rd_poster.git"
 fdcl_repo="git@github.com:fdcl-gwu/2017_RD_poster.git"
 
